1a95a8fbbd1d1b10195f640b6904e44cacfc93c4
Anton Einarson <anton.einarson@gmail.com>
2016-02-15 20:34:16 +0100

    woho we have outydata

--- guiMain.py
+++ guiMain.py
@@ -1,12 +1,33 @@
 from Tkinter import *
 import Utils
 import logging as log
+import FileManager
+
+
+
+
+def getFiles():
+    global paths
+    paths.set(FileManager.getAllFiles("./"))
 
 def buildGUI():
+    global paths
     log.debug("build GUI ...")
     root = Tk()
+    root.title("BreainSweat")
 
-    Button(root, text="BrainSweat").grid()
+
+    paths = StringVar()
+
+    mainFrame = Frame(root)
+    mainFrame.grid(column=0, row=0, sticky=(N, W, E, S))
+    mainFrame.columnconfigure(0, weight=1)
+    mainFrame.rowconfigure(0, weight=1)
+
+
+    Label(mainFrame, textvariable=paths).grid(column=0,row=0, sticky=(W, E))
+
+    Button(mainFrame, text="Load", command=getFiles).grid(column=0,row=1, sticky=(W, E))
 
     log.info("GUI Done!")
     return root
