4e6e735a0a9efb8993d4a168af89fa9522f0d79d
Anton Einarson <anton.einarson@gmail.com>
2016-02-15 19:57:33 +0100

    abs path now

--- FileManager.py
+++ FileManager.py
@@ -2,14 +2,15 @@ import os
 import logging.config
 from logging import *
 
-
 # option to exclude things
 def getAllFiles(path):
     f = []
-
+    info("Retrives files")
     for (dirpath, dirnames, filenames) in os.walk(path):
         for filename in filenames:
-            f.append(dirpath + filename)
+            absPath = os.path.abspath(dirpath + filename)
+            info("File: {0}".format(absPath))
+            f.append(absPath)
     return f
 
 if __name__ == "__main__":
