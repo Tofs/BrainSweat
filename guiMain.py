d5b7c48a3e29031adbffd2c2ef2888bc87f7ffc8
Anton Einarson <anton.einarson@gmail.com>
2016-02-15 20:09:34 +0100

    basic tkinter window this some form of loggins

--- guiMain.py
+++ guiMain.py
@@ -1,6 +1,10 @@
 import Tkinter
-
-
+import Utils
+import logging as log
 
 if __name__ == "__main__":
-    print "hello world"
+    Utils.initLogger()
+    log.info("Program start")
+    top = Tkinter.Tk()
+    top.mainloop()
+    log.info("Program stop")
